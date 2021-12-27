a50f2250ee610c2f9768480a1126e33894d5e8bc
sierozem <sierozem0@yahoo.co.jp>
2021-12-27 18:08:50 +0900

    Tailwind CSSのThemeを設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,29 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange': '#ffa355',
+        'light-orange': '#ffa400',
+        'deep-orange': '#fd8c29',
+        'cream': '#fffbf2',
+      },
+      fontFamily: {
+        'sans': ["'M Plus Rounded 1c'", 'sans-serif'],
+      },
+      boxShadow: {
+        'flat': '0 3px 0 rgba(0, 0, 0, .06)',
+      },
+      backgroundImage: {
+        'wave-top': "url('/wave_top.svg'), url('/wave_top.svg')",
+        'wave-bottom': "url('/wave_bottom.svg'), url('/wave_bottom.svg')",
+      },
+      backgroundPosition: {
+        'layerd-top': '0 bottom, 100px bottom',
+        'layerd-bottom': '0 top, 100px top',
+      },
+    },
   },
-  plugins: [],
 }
